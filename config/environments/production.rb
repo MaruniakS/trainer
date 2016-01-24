69e75b7b1f98392e1db3b54ef1c1bfc1bf1012eb
STASILu4 <stasilu4@gmail.com>
2016-01-24 16:04:23 +0200

    heroku fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address => 'smtp.gmail.com',
       :port => 587,
-      :domain => 'gmail.com',
+      :domain =>  ENV['gmail.com'],
       :user_name => ENV['trainer.sup@gmail.com'],
       :password => ENV['trainer-support'],
       :authentication => :plain,
