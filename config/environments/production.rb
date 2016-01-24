337d4a05d46dfc96e6f2461e0ccdd407d8af8c86
STASILu4 <stasilu4@gmail.com>
2016-01-24 15:53:39 +0200

    production name added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'trainer-assistant.herokuapp.com' }
   config.action_mailer.smtp_settings = {
-      :address => "127.0.0.1",
-      :port    => 25,
-      :domain  => 'yourdomain.com'
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :domain => 'gmail.com',
+      :user_name => ENV['trainer.sup@gmail.com'],
+      :password => ENV['trainer-support'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
   }
   # Code is not reloaded between requests.
   config.cache_classes = true
