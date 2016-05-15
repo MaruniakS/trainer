8e1fb2a8e7d729256a23707436e160a707ae0d9b
STASILu4 <stasilu4@gmail.com>
2016-05-15 23:46:08 +0300

    training days tab

--- app/models/training_day.rb
+++ app/models/training_day.rb
@@ -1,4 +1,8 @@
 class TrainingDay < ActiveRecord::Base
   has_many :training_day_exercises
   has_many :exercises, through: :training_day_exercises
+
+  def has_exercises?
+    exercises.count > 0 ? true : false
+  end
 end
