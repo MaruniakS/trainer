974f75b8265d73720d2a3834f8ff209d3655f9b7
STASILu4 <stasilu4@gmail.com>
2016-05-19 21:47:20 +0300

    about

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,23 +11,40 @@ $(document).ready(function(){
     );
 
     $("#main-content-1, #main-content-2, #main-content-3, #content-image-1,#content-image-2,#content-image-3").css('opacity', 0);
+    $("#time-line-1, #time-line-2, #time-line-3, #time-line-4,#time-line-5").css('opacity', 0);
     $('#main-content-1').waypoint(function() {
         $('#main-content-1').addClass('animated fadeInLeft');
-    }, { offset: '50%' });
+    }, { offset: '70%' });
     $('#main-content-3').waypoint(function() {
         $('#main-content-3').addClass('animated fadeInLeft');
-    }, { offset: '50%' });
+    }, { offset: '70%' });
     $('#main-content-2').waypoint(function() {
         $('#main-content-2').addClass('animated fadeInRight');
-    }, { offset: '50%' });
+    }, { offset: '70%' });
 
     $('#content-image-1').waypoint(function() {
         $('#content-image-1').addClass('animated fadeInRight');
-    }, { offset: '50%' });
+    }, { offset: '70%' });
     $('#content-image-2').waypoint(function() {
         $('#content-image-2').addClass('animated fadeInLeft');
-    }, { offset: '50%' });
+    }, { offset: '70%' });
     $('#content-image-3').waypoint(function() {
         $('#content-image-3').addClass('animated fadeInRight');
-    }, { offset: '50%' });
+    }, { offset: '70%' });
+
+    $('#time-line-1').waypoint(function() {
+        $('#time-line-1').addClass('animated fadeInDown');
+    }, { offset: '100%' });
+    $('#time-line-2').waypoint(function() {
+        $('#time-line-2').addClass('animated fadeInDown');
+    }, { offset: '100%' });
+    $('#time-line-3').waypoint(function() {
+        $('#time-line-3').addClass('animated fadeInDown');
+    }, { offset: '100%' });
+    $('#time-line-4').waypoint(function() {
+        $('#time-line-4').addClass('animated fadeInDown');
+    }, { offset: '100%' });
+    $('#time-line-5').waypoint(function() {
+        $('#time-line-5').addClass('animated fadeInDown');
+    }, { offset: '100%' });
 });
